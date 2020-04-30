6d3a881004120c7730f35602a15f296994090ab7
cakers <chris.j.akers@gmail.com>
2020-04-30 15:16:46 +0100

    Changed all indents to tabs (some were spaces)

--- chapter-4-ex-1/tee.c
+++ chapter-4-ex-1/tee.c
@@ -30,19 +30,19 @@
 
 int main(int argc, char** argv) {
 	
-    if (cakelog_initialise("tlpi-tee") != 0) {
-        printf("main() : error initialising logging. Fast fail.\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    void* buf[BUF_SIZE];
+	if (cakelog_initialise("tlpi-tee") != 0) {
+		printf("main() : error initialising logging. Fast fail.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	void* buf[BUF_SIZE];
 	int file_append = false;
 
 	// Parse options
 	cakelog("Parsing options");
 	
-    int opt;
-    while ((opt = getopt(argc, argv, "a")) != -1) {
+	int opt;
+	while ((opt = getopt(argc, argv, "a")) != -1) {
 		if ((unsigned char)opt == 'a') {
 			cakelog("Option [a] found (APPEND)");
 			file_append = true;
@@ -72,27 +72,27 @@ int main(int argc, char** argv) {
 		file_open_flags = (file_open_flags | O_EXCL);
 	}
 
-    int outfile_fd = open(out_file, file_open_flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	int outfile_fd = open(out_file, file_open_flags, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	cakelog("Opened outfile [%s] successfully with file descriptor [%ld]",out_file, outfile_fd);
-    
-    if (outfile_fd == -1) {
+
+	if (outfile_fd == -1) {
 		perror("caketee (outfile)");
 		exit(EXIT_FAILURE);
-    }
-    
-    ssize_t bytes_read;
-    ssize_t bytes_written_stdout;
+	}
+
+	ssize_t bytes_read;
+	ssize_t bytes_written_stdout;
 	ssize_t bytes_written_outfile;
-    while ((bytes_read = read(STDIN_FILENO, buf, BUF_SIZE+1)) > 0) {
+	while ((bytes_read = read(STDIN_FILENO, buf, BUF_SIZE+1)) > 0) {
 		cakelog("Read [%ld] bytes from STDIN", bytes_read);
 		bytes_written_stdout = write(STDOUT_FILENO, buf, bytes_read);
 		cakelog("Written [%ld] bytes to STDOUT", bytes_written_stdout);
 		bytes_written_outfile = write(outfile_fd, buf, bytes_read);
 		cakelog("Written [%ld] bytes to outfile", bytes_written_outfile);
-    }
+	}
 
-    close(outfile_fd);
+	close(outfile_fd);
 	cakelog_stop();
-    return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
 
